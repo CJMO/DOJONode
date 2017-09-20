f25e3e8081a6add75ff1a9339edc31bec0cf53c1
CJMO <cristian.m.u@hotmail.com>
2017-09-20 17:46:21 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,3 @@
-/*var http = require('http');
-var fs = require('fs');
-
-const PORT=8080; 
-
-fs.readFile('./index.html', function (err, html) {
-
-    if (err) throw err;    
-
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
-});*/
-
 var http        = require('http');
 var url         = require('url');
 var querystring = require('querystring');
